feb3b4a4e3b431e8866c1948ef1eaa0fcab632b0
Yuliia Boiko <littlehappinies@gmail.com>
2022-05-05 19:05:49 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,56 @@
+import Notiflix from 'notiflix';
+import "notiflix/dist/notiflix-3.2.5.min.css";
+
+const form = document.querySelector('.form');
+const amountInput = form.elements.amount;
+const delayInput = form.elements.delay;
+const stepInput = form.elements.step;
+
+let promiseCounter = 0;
+let delay = 0;
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  callPromises(amountInput.value);
+};
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      }
+
+      reject(`❌ Rejected promise ${position} in ${delay}ms`);
+    }, delay);
+  });
+};
+
+function onCreatePromiseSuccess(result) {
+  Notiflix.Notify.success(result);
+};
+
+function onCreatePromiseError(error) {
+  Notiflix.Notify.failure(error);
+};
+
+function callPromises(amount) {
+  delay = Number(delayInput.value) - Number(stepInput.value);
+
+  for (let i = 0; i < amount; i++) {
+    promiseCounter += 1;
+    delay += Number(stepInput.value);
+
+    createPromise(promiseCounter, delay)
+      .then(onCreatePromiseSuccess).catch(onCreatePromiseError);
+  };
+};
+
+
+
